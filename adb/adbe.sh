3f090c3356d859e98f8717f8e3de6bd87427705b
Nishant Srivastava <nisrulz@gmail.com>
2018-07-07 18:13:05 +0200

    added adbe script which allows to target multiple connected devices and run adb commands on each one of them

---
+++ adb/adbe.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Script adbe
+
+# Requirements
+# adb : It should be in your path so you can access it from anywhere
+# awk: The script uses awk tool. Have it in the path too so you can access it.
+#
+# Usage
+# Run any command you would run with adb for all the connected devices
+# ./adbe <command> is the equivalent of ./adb -s <serial number> <command>
+#
+# Examples
+# ./adbe version
+# ./adbe install demo_app.apk
+# ./adbe uninstall com.example.company.android
+
+# If you would like to use adbe from any where create an alias to this script
+# in your bashrc or zshrc file
+
+adb devices | while read line
+do
+    if [ ! "$line" = "" ] && [ `echo $line | awk '{print $2}'` = "device" ]
+    then
+        device=`echo $line | awk '{print $1}'`
+        echo "$device $@ ..."
+        adb -s $device $@
+    fi
+done
